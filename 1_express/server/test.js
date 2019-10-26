b1973911f6adbf9de5f13c72f866cb7de07564ee
deepcrazy <312.gupta@gmail.com>
2019-10-26 19:44:40 -0400

    modified index html file and added few comments in test.js file

--- 1_express/server/test.js
+++ 1_express/server/test.js
@@ -1,15 +1,15 @@
 try {
-    const port = 5003;
-    const express = require('express');
-    const app = express();
+    const port = 5003;                      // Set the port
+    const express = require('express');     
+    const app = express();                  // Use of express dependency
 
     app.get('/', (req, res) => {
-        res.send('express.js dependency has been installed successfully..!!')
+        res.send('express.js dependency has been installed successfully..!!');  //  Human readable message to be sent to the browser
     });
 
     app.listen(port, () => {
         console.log('ok on ' + port);
     });
 } catch (error) {
-    console.log("Error occured: " + error);
+    console.log("Error occured: " + error);     //  Error handling
 }
