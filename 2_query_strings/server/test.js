c66a77ddea9721a25f3464008dbd2e0b4240873b
deepcrazy <312.gupta@gmail.com>
2019-10-26 20:33:09 -0400

    Modified test.js for no query string data message and added few comments also

--- 2_query_strings/server/test.js
+++ 2_query_strings/server/test.js
@@ -4,21 +4,22 @@ try {
     const port = 1080;
     const url = require('url');
 
+    //  Creating the server
     const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        var urlRequestedData = url.parse(req.url, true).query;
+        var urlRequestedData = url.parse(req.url, true).query;  //  Split URl into parts
 
         var d = new Date();
-        // var returnValue = res.write("Date and Day: " + d.toDateString() + "<br/>" + "Time: " + d.toLocaleTimeString());
+        //  Day, Date and Time
         var returnValue = "Day and Date: " + d.toDateString() + "\nTime: " + d.toLocaleTimeString();
-
+        //  Url requested
         returnValue = url.parse(req.url, true).search != null ? returnValue + "\nUrl requested: " + "http://" + hostname + ":" + port + "/" + url.parse(req.url, true).search : returnValue + "\nUrl requested: " + "http://" + hostname + ":" + port;
-
-        returnValue = Object.keys(urlRequestedData).length != 0 ? returnValue + "\nQuery String Data: " + JSON.stringify(urlRequestedData) : returnValue;
+        //  Names and Values of query data along with handling of no query string data
+        returnValue = Object.keys(urlRequestedData).length != 0 ? returnValue + "\nQuery String Data: " + JSON.stringify(urlRequestedData) : returnValue + "\nNo Query String entered";
 
         res.end(returnValue);
     }).listen(port, () => {
-        console.log('http://'+ hostname + ':' + port + '    ok');
+        console.log('http://'+ hostname + ':' + port + '    ok');   //  Error Handling
     });
 
 } catch (error) {
